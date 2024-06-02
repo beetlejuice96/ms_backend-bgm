7eb53bf56877ca5cb18b1576daa342e660cdc8bf
beetlejuice96 <dilanbernbae043@gmail.com>
2024-06-02 19:38:06 -0300

    add logger module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import { LoggerModule } from './logger/logger.module';
+
+import config from '@/config/env.config';
+import { validationSchema } from '@/config/env.validation-schema';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      load: [config],
+      isGlobal: true,
+      validationSchema,
+    }),
+    ,
+    LoggerModule,
+  ],
   controllers: [],
   providers: [],
 })
